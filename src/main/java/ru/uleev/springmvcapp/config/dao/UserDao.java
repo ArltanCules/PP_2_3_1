4999ad55dd5e4ad270e9c7d19cb78c86008d3cee
ArltanCules <106382293+ArltanCules@users.noreply.github.com>
2022-09-23 04:19:52 +0300

    не работает валидация

--- src/main/java/ru/uleev/springmvcapp/config/dao/UserDao.java
+++ src/main/java/ru/uleev/springmvcapp/config/dao/UserDao.java
@@ -1,47 +1,50 @@
 package ru.uleev.springmvcapp.config.dao;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import ru.uleev.springmvcapp.config.model.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class UserDao {
-    private static int peopleCount;
-    private List<User> users;
 
-    {
-        users = new ArrayList<>();
-        users.add(new User(++peopleCount, "Tom",24,"tomsoyer@mail.ru"));
-        users.add(new User(++peopleCount, "Bill",36,"bolokon@mail.ru"));
-        users.add(new User(++peopleCount, "Mikhael",48,"iomsoyer@mail.ru"));
-        users.add(new User(++peopleCount, "Robert",39,"yasaasr@mail.ru"));
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public UserDao(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
+    @Transactional
     public List<User> index() {
+        Session session = sessionFactory.getCurrentSession();
+        List<User> users=session.createQuery("select p from User p", User.class)
+                .getResultList();
+
         return users;
+
     }
 
     public User show(int id) {
-        return users.stream().filter(user -> user.getId() == id).findAny().orElse(null);
+
+        return null;
     }
 
     public void save(User user) {
-        user.setId(++peopleCount);
-        users.add(user);
+
     }
 
-    public void update(int id,User updateUser) {
-        User userToBeUpdated = show(id);
+    public void update(int id, User updateUser) {
 
-        userToBeUpdated.setName(updateUser.getName());
-        userToBeUpdated.setAge(updateUser.getAge());
-        userToBeUpdated.setEmail(updateUser.getEmail());
     }
 
     public void delete(int id) {
-        users.removeIf(u -> u.getId()==id);
+
     }
 
 }
--- src/main/java/ru/uleev/springmvcapp/config/model/User.java
+++ src/main/java/ru/uleev/springmvcapp/config/model/User.java
@@ -1,20 +1,28 @@
 package ru.uleev.springmvcapp.config.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column(name ="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private int id;
 
     @NotEmpty(message = "Name should not be empty")
     @Size(min=2,max = 30,message = "Name should be between 2 and 30 characters")
+    @Column(name ="name")
     private String name;
     @Min(value = 0,message = "Age should be greater than 0")
+    @Column(name="age")
     private int age;
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
+    @Column(name="email")
     private String email;
 
     public User() {}
